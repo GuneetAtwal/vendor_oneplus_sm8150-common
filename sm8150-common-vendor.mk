c1ee5b998f3f468c3f59274b7cc21b399732f93d
Quince <quinceroms@gmail.com>
2026-02-21 10:16:00 +0100

    sm8150-common: Add missing camera image processing blobs
    Add camera libraries that are dynamically loaded (dlopen) by existing
    camera blobs but were missing from the vendor tree:
    
    - libSTDualCamCaptureWT: SenseTime dual-cam snapshot fusion
    - libtriplecam_image_optical_zoom: triple-cam image optical zoom
    - lib_oneplus_watermark: photo watermark rendering
    - libsdk_sr: super resolution processing
    - libst_relighting: SenseTime portrait relighting
    - libSuperSensor{,Fallback,Processor,ProcessorCWrapper}: SuperSensor pipeline
    
    These are common to all SM8150 models (OP7/7Pro/7T/7TPro) and are loaded
    by libopcamera_native_modules.so and com.oneplus.node.snapshotfusion.so.
    
    Source: OnePlus7-user-12-SKQ1.211113.001-P.202303230244-release-keys
    Signed-off-by: Quince <quinceroms@gmail.com>

--- sm8150-common-vendor.mk
+++ sm8150-common-vendor.mk
@@ -495,9 +495,15 @@ PRODUCT_PACKAGES += \
     libQSEEComAPI \
     libQTEEConnector_vendor \
     libSecureUILib \
+    libSTDualCamCaptureWT \
     libSonyIMX471RmscLibrary \
     libStDrvInt \
+    libSuperSensor \
+    libSuperSensorFallback \
+    libSuperSensorProcessor \
+    libSuperSensorProcessorCWrapper \
     lib_bokehlib \
+    lib_oneplus_watermark \
     libadsp_default_listener \
     libaoa \
     libarcsoft_beautyshot \
@@ -613,6 +619,7 @@ PRODUCT_PACKAGES += \
     libsensorcal \
     libsensorslog \
     libsettings \
+    libsdk_sr \
     libsns_device_mode_stub \
     libsns_fastRPC_util \
     libsns_low_lat_stream_stub \
@@ -624,10 +631,12 @@ PRODUCT_PACKAGES += \
     libssc_default_listener \
     libssd \
     libstblur_capture_api \
+    libst_relighting \
     libswregistrationalgo \
     libsystem_health_mon \
     libtime_genoff \
     libtinyxml2_1 \
+    libtriplecam_image_optical_zoom \
     libtriplecam_optical_zoom_control \
     libtriplecam_video_optical_zoom \
     libtrustedapploader \
