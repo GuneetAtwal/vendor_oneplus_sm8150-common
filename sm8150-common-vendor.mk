e9194853828976fd3964742f85b2f11a1dbcca1f
Codecity001 <sanganiharish263@gmail.com>
2026-02-24 19:35:58 -0600

    [OOSCam] sm8150-common: Add more Image processing blobs * libSTDualCamCaptureWT * libSuperSensor * libSuperSensorFallback * libSuperSensorProcessor * libsdk_sr * libst_relighting * libtriplecam_image_optical_zoom * libSuperSensorProcessorCWrapper * Also add oplus_google_lens_config
    Change-Id: I405961e334230125bd01af857ca9cc9c652ba711
    Signed-off-by: Codecity001 <sanganiharish263@gmail.com>

--- sm8150-common-vendor.mk
+++ sm8150-common-vendor.mk
@@ -265,6 +265,7 @@ PRODUCT_COPY_FILES += \
     vendor/oneplus/sm8150-common/proprietary/product/etc/permissions/com.oneplus.camera.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.oneplus.camera.xml \
     vendor/oneplus/sm8150-common/proprietary/product/etc/permissions/com.oneplus.camera.service.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.oneplus.camera.service.xml \
     vendor/oneplus/sm8150-common/proprietary/product/etc/permissions/com.oneplus.gallery.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.oneplus.gallery.xml \
+    vendor/oneplus/sm8150-common/proprietary/product/etc/permissions/oplus_google_lens_config.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/oplus_google_lens_config.xml \
     vendor/oneplus/sm8150-common/proprietary/product/etc/sysconfig/oneplus-hiddenapi.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/oneplus-hiddenapi.xml
 
 PRODUCT_PACKAGES += \
@@ -606,6 +607,7 @@ PRODUCT_PACKAGES += \
     librilqmiservices \
     librpmb \
     libsdedrm \
+    libsdk_sr \
     libsdm-color \
     libsdm-colormgr-algo \
     libsdm-diag \
@@ -627,10 +629,17 @@ PRODUCT_PACKAGES += \
     libssc_default_listener \
     libssd \
     libstblur_capture_api \
+    libst_relighting \
+    libSTDualCamCaptureWT \
+    libSuperSensor \
+    libSuperSensorFallback \
+    libSuperSensorProcessor \
+    libSuperSensorProcessorCWrapper \
     libswregistrationalgo \
     libsystem_health_mon \
     libtime_genoff \
     libtinyxml2_1 \
+    libtriplecam_image_optical_zoom \
     libtriplecam_optical_zoom_control \
     libtriplecam_video_optical_zoom \
     libtrustedapploader \
